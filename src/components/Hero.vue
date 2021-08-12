910a0efd284477a44b0fe645906283a703f54fa8
sicelo17 <70627871+sicelo17@users.noreply.github.com>
2021-08-12 16:35:16 +0200

    added carousel

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,12 +1,87 @@
 <template>
+<section class="section">
+      <div class="hero-section">
+      <div class="bd-container">
+      <agile :dots="true" :autoplay="true" :infinite="true" :autoplay-speed="8000" :speed="2500" :navButtons="false" fade :pauseOnHover="false" >
+    <div class="slide">
+     <div class="hero-1">
+         evsdfcevf
+     </div>
+    </div>
 
+    <div class="slide">
+      <div class="hero-2">
+gfdsvf
+      </div>
+    </div>
+  </agile>
+  </div>
+  </div>
+</section>
 </template>
 
 <script>
+import { VueAgile } from "vue-agile";
 export default {
-  name: "Hero",
+  components: {
+    agile: VueAgile,
+  },
 };
 </script>
 
-<style scoped>
+<style>
+.agile__dots {
+  bottom: 10px;
+  flex-direction: column;
+  right: 30px;
+  position: absolute;
+}
+
+.agile__dot {
+  margin: 5px 0;
+}
+
+.agile button {
+  background: transparent;
+  border: 1px solid #fff;
+  cursor: pointer;
+  display: block;
+  height: 10px;
+  font-size: 0;
+  line-height: 0;
+  margin: 0;
+  padding: 0;
+  transition-duration: 0.3s;
+  width: 10px;
+}
+
+.agile button:hover {
+    background-color: var(--white);
+}
+
+img {
+    object-fit: cover;
+}
+
+.slide {
+    display: block;
+	height: 500px;
+	object-fit: cover;
+	width: 100%;
+}
+
+.hero-1 {
+    background-image: url("../assets/business.jpeg");
+    background-repeat: no-repeat;
+    background-size: cover;
+    width: 100%;
+    height: 100vh;
+}
+
+.hero-2 {
+    background-image: url("../assets/logo.png");
+    width: 100%;
+    height: 100vh;
+}
+
 </style>
