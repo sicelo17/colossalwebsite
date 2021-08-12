e3a1060564c50849d0665abb8b7a8647a909ec95
sicelo17 <70627871+sicelo17@users.noreply.github.com>
2021-08-12 11:09:32 +0200

    imported header to app

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,20 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div>
+    <Header />
+    
   </div>
   <router-view/>
 </template>
 
+<script>
+import Header from './components/Header.vue'
+
+export default {
+  components: {
+    Header
+  }
+}
+</script>
 <style>
 /* ==== GOOGLE FONTS ==== */
 @import url("https://fonts.googleapis.com/css2?family=EB+Garamond:ital,wght@0,400;0,500;0,700;1,400;1,500;1,600;1,700&family=Poppins:wght@400;500;600&display=swap");
