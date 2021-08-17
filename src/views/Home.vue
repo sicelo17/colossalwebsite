0ce7410027751b6d425503a0d4366ee327c4ac34
sicelo17 <70627871+sicelo17@users.noreply.github.com>
2021-08-17 19:35:28 +0200

    added cards styling

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,18 +2,21 @@
   <div>
     <Hero />
     <Services />
+    <News />
   </div>
 </template>
 
 <script>
 import Hero from "../components/Hero.vue"
 import Services from "../components/Services.vue"
+import News from "../components/News.vue"
 
 export default {
   name: 'Home',
   components: {
   Hero,
   Services,
+  News,
   }
 }
 </script>
