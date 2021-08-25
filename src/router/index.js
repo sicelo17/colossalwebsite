8db88c34d55460d457e9171d4be3935f8e4c7de7
sicelo17 <70627871+sicelo17@users.noreply.github.com>
2021-08-25 19:37:19 +0200

    added nav routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import Industries from '../views/Industries.vue'
+import Services from '../views/Services.vue'
+import Products from '../views/Products.vue'
 
 const routes = [
   {
@@ -14,7 +17,22 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  },
+  {
+    path: '/industries',
+    name: 'Industries',
+    component: Industries
+  },
+  {
+    path: '/services',
+    name: 'Services',
+    component: Services
+  },
+  {
+    path: '/products',
+    name: 'Products',
+    component: Products
+  },
 ]
 
 const router = createRouter({
---
+++ src/views/Industries.vue
@@ -0,0 +1,15 @@
+<template>
+  <section class="section">
+      Industries
+  </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
