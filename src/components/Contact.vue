fcc82211c3197b446f772de4907af3d4cdfa5882
sicelo17 <70627871+sicelo17@users.noreply.github.com>
2021-09-04 14:20:49 +0200

    edited contact section

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,20 +1,19 @@
 <template>
 
-    <div class="contact">
-      <div class="heading">
-        <h1>Interested in working with us?</h1>
-        <h2>Quality software guaranteed</h2>
+   <div class="contact">
+        <div class="bd-container">
+        <div class="contact__content">
+          <div class="contact__data">
+            <h3>Interested in working with us</h3>
+            <p>Quality software guaranteed</p>
+          </div>
+          <form >
+            <input type="email" placeholder="Enter your email address" class="contact__email">
+            <a class="contact__link" href="#">Request Demo</a>
+          </form>
+        </div>
       </div>
-
-      <div class="buttons">
-        <router-link to="/" class="button_link"
-          ><button class="button request">Request Demo</button></router-link
-        >
-        <router-link to="/trial" class="button_link"
-          ><button class="button trial">Free Trial</button></router-link
-        >
-      </div>
-    </div>
+   </div>
 </template>
 
 <script>
@@ -23,63 +22,129 @@ export default {};
 
 <style scoped>
 .contact {
-  text-align: center;
-  background: white;
-  font-style: italic;
-  margin-bottom: var(--mb-4);
-  line-height: 1.2;
-}
-.heading {
-  margin-bottom: var(--mb-3);
-  padding-top: 1rem;
-}
-.buttons {
-  display: flex;
-  justify-content: space-around;
-  padding-bottom: var(--mb-4);
-}
-
-.button {
-  font-style: italic;
-  font-size: var(--h2-font-size);
-  font-family: var(--body-font);
-  font-weight: var(--font-semi-bold);
-}
-.button:hover {
-  background: linear-gradient(to bottom right, var(--blue), var(--turquoise));
-  color: var(--white);
-  transition: 0.2s ease-in all;
-}
-.request {
+  padding: var(--biggest-font-size) 0;
   background-color: var(--white);
+}
+
+.contact__content {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.contact__data h3 {
+  font-size: var(--h1-font-size);
+  font-weight: inherit;
+}
+
+.contact__data p {
+  font-size: var(--normal-font-size);
   color: var(--blue);
-  border: 2px solid var(--blue);
-  padding: 1rem 2rem;
 }
 
-.trial {
-  background: var(--blue);
-  color: var(--turquoise);
-  border: 2px solid var(--turquoise);
-  padding: 1rem 3rem;
+.contact__email {
+  font-size: 1rem;
+  display: inline-block;
+  width: 30vw;
+  padding: 1rem;
+  background-color: var(--off-white);
+  border: none;
+  text-indent: 1rem;
 }
 
-/* ===== RESPONSIVE DESIGN ===== */
+.contact__email:focus {
+  outline: none;
+}
 
-@media only screen and (max-width: 520px) {
-  .heading {
-    margin: 0;
+.contact__link:link,
+.contact__link:visited {
+  display: inline-block;
+  padding: 0.8rem 3rem;
+  margin-left: -0.5rem;
+  background-color: var(--blue);
+  color: var(--white);
+  transition: 0.3s;
+}
+
+.contact__link:hover {
+  background-color: var(--turquoise);
+  color: var(--blue);
+}
+
+/*
+======================
+contact Media Query
+======================
+*/
+@media only screen and (max-width: 998px) {
+  .contact {
+    padding: 3rem 2rem;
   }
-  .buttons {
+
+  .contact__content {
     flex-direction: column;
   }
 
-  .button {
-    margin-top: 2rem;
+  .contact__email {
+    width: 40rem;
   }
 
-  .button:last-child {
-    margin-bottom: 0;
+  .contact__data {
+    margin-bottom: 2rem;
+  }
+
+  .contact__link:link,
+  .contact__link:visited {
+    margin-left: 40%;
+    margin-top: var(--h2-font-size);
   }
 }
+
+@media only screen and (max-width: 768px) {
+  .contact__content {
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+  }
+
+  .contact__email {
+    width: 30rem;
+    display: block;
+    margin-bottom: 1.6rem;
+  }
+
+  .contact__link:link,
+  .contact__link:visited {
+    margin-left: 0;
+    margin-top: var(--h2-font-size);
+  }
+}
+
+@media only screen and (max-width: 568px) {
+  .contact__email {
+    width: 30rem;
+    font-size: 1rem;
+  }
+
+  .contact__data h3 {
+    font-size: 1.6rem;
+  }
+
+  .contact__data p {
+    font-size: 1rem;
+  }
+
+  .contact__link:link,
+  .contact__link:visited {
+    padding: 1.2rem 2rem;
+    margin-left: 0;
+  }
+}
+
+@media only screen and (max-width: 480px) {
+  .contact__email {
+    width: 20rem;
+  }
+}
+
 </style>
