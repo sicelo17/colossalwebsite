b5343b24c5f019499210431f2f55b040c8a65583
sicelo17 <70627871+sicelo17@users.noreply.github.com>
2021-09-07 10:18:31 +0200

    made industries responsive

--- src/views/Industries.vue
+++ src/views/Industries.vue
@@ -1,101 +1,93 @@
 <template>
   <section class="section">
-    <div class="industries-page bd-grid">
+    <div class="industries-page">
       <div class="bd-container">
         <div class="industry-title">
           <h1>We serve the following industries</h1>
         </div>
-        <div class="industry-items">
-          <div class="industry">
-            <div class="ind">
-              <router-link to="/fashion">
-                <span><i class="bx bxs-t-shirt"></i></span>
-                <p>Fashion</p>
-              </router-link>
-            </div>
-
-            <div class="ind">
-              <router-link to="/manufacturing">
-                <span><i class="bx bxs-factory"></i></span>
-                <p>Manufacturing</p>
-              </router-link>
-            </div>
-
-            <div class="ind">
-              <router-link to="/retail">
-                <span><i class="bx bxl-shopify"></i></span>
-                <p>Retail</p>
-              </router-link>
-            </div>
+        <div class="industry-items bd-grid">
+          <div class="ind">
+            <router-link to="/fashion">
+              <span><i class="bx bxs-t-shirt"></i></span>
+              <p>Fashion</p>
+            </router-link>
           </div>
 
-          <div class="industry">
-            <div class="ind">
-              <router-link to="/consulting">
-                <span><i class="bx bxs-group"></i></span>
-                <p>Consulting</p>
-              </router-link>
-            </div>
-
-            <div class="ind">
-              <router-link to="/education">
-                <span><i class="bx bxs-graduation"></i></span>
-                <p>Education</p>
-              </router-link>
-            </div>
-
-            <div class="ind">
-              <router-link to="/foodbeverage">
-                <span><i class="bx bxs-coffee-alt"></i></span>
-                <p>Food & Beverage</p>
-              </router-link>
-            </div>
+          <div class="ind">
+            <router-link to="/manufacturing">
+              <span><i class="bx bxs-factory"></i></span>
+              <p>Manufacturing</p>
+            </router-link>
           </div>
 
-          <div class="industry">
-            <div class="ind">
-              <router-link to="/restaurants">
-                <span><i class="bx bx-restaurant"></i></span>
-                <p>Restaurants</p>
-              </router-link>
-            </div>
-
-            <div class="ind">
-              <router-link to="/it-services">
-                <span><i class="bx bxl-visual-studio"></i></span>
-                <p>IT Services</p>
-              </router-link>
-            </div>
-
-            <div class="ind">
-              <router-link to="/realestate">
-                <span><i class="bx bxs-home"></i></span>
-                <p>Real Estate</p>
-              </router-link>
-            </div>
+          <div class="ind">
+            <router-link to="/retail">
+              <span><i class="bx bxl-shopify"></i></span>
+              <p>Retail</p>
+            </router-link>
           </div>
 
-          <div class="industry">
-            <div class="ind">
-              <router-link to="/professionalservices">
-                <span><i class="bx bxs-user-check"></i></span>
-                <p>Professional Services</p>
-              </router-link>
-            </div>
+          <div class="ind">
+            <router-link to="/consulting">
+              <span><i class="bx bxs-group"></i></span>
+              <p>Consulting</p>
+            </router-link>
+          </div>
 
-            <div class="ind">
-              <router-link to="/transportation">
-                <span><i class="bx bxs-truck"></i></span>
-                <p>Transportation</p>
-              </router-link>
-            </div>
+          <div class="ind">
+            <router-link to="/education">
+              <span><i class="bx bxs-graduation"></i></span>
+              <p>Education</p>
+            </router-link>
+          </div>
 
-            <div class="ind">
-              <router-link to="/wholesale">
-                <span><i class="bx bxs-building-house"></i></span>
-                <p>Wholesale</p>
-              </router-link>
-            </div>
+          <div class="ind">
+            <router-link to="/foodbeverage">
+              <span><i class="bx bxs-coffee-alt"></i></span>
+              <p>Food & Beverage</p>
+            </router-link>
+          </div>
+
+          <div class="ind">
+            <router-link to="/restaurants">
+              <span><i class="bx bx-restaurant"></i></span>
+              <p>Restaurants</p>
+            </router-link>
+          </div>
+
+          <div class="ind">
+            <router-link to="/it-services">
+              <span><i class="bx bxl-visual-studio"></i></span>
+              <p>IT Services</p>
+            </router-link>
+          </div>
+
+          <div class="ind">
+            <router-link to="/realestate">
+              <span><i class="bx bxs-home"></i></span>
+              <p>Real Estate</p>
+            </router-link>
+          </div>
+
+          <div class="ind">
+            <router-link to="/professionalservices">
+              <span><i class="bx bxs-user-check"></i></span>
+              <p>Professional Services</p>
+            </router-link>
+          </div>
+
+          <div class="ind">
+            <router-link to="/transportation">
+              <span><i class="bx bxs-truck"></i></span>
+              <p>Transportation</p>
+            </router-link>
+          </div>
+
+          <div class="ind">
+            <router-link to="/wholesale">
+              <span><i class="bx bxs-building-house"></i></span>
+              <p>Wholesale</p>
+            </router-link>
           </div>
         </div>
       </div>
@@ -112,33 +104,25 @@ export default {
 </script>
 
 <style scoped>
-
 .section {
-  background: linear-gradient(to top left, rgba(72, 140, 228, 0.897), var(--turquoise));
+  background: linear-gradient(
+    to top left,
+    rgba(72, 140, 228, 0.897),
+    var(--turquoise)
+  );
 }
 
 span {
   font-size: 4rem;
-  padding: .5rem;
+  padding: 0.5rem;
   border-radius: 1rem;
   background: var(--white);
 }
 
 .industry-items {
-  grid-template-columns: 1fr;
+  grid-template-columns: repeat(3, 1fr);
   text-align: center;
   margin: 2rem 0;
-}
-
-.industry {
-  display: flex;
-  align-items: center;
-  justify-content: space-around;
-  padding: 1.5rem;
-}
-
-.ind:not(:last-child) {
-  margin-right: 8rem;
 }
 
 .ind:hover {
@@ -149,5 +133,13 @@ span {
   font-size: var(--h1-font-size);
   text-align: center;
   font-style: italic;
+}
+
+/* ==== RESPONSIVE DESIGN ==== */
+
+@media only screen and (max-width: 500px) {
+  .industry-items {
+    grid-template-columns: repeat(2, 1fr);
+  }
 }
 </style>
