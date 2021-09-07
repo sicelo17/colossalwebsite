8f28a0c8ec105b209a3b75dae589b4110adeb24e
sicelo17 <70627871+sicelo17@users.noreply.github.com>
2021-09-07 10:10:50 +0200

    made products responsive

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -1,11 +1,10 @@
 <template>
   <section class="section">
-      <div class="bd-container">
-        <div class="product-heading">
-          <h1>Some of our products</h1>
-        </div>
-        <div class="products-page bd-grid">
-        <div class="products">
+    <div class="bd-container">
+      <div class="product-heading">
+        <h1>Some of our products</h1>
+      </div>
+      <div class="products-page bd-grid">
           <div class="product">
             <router-link to="/crm">
               <span class="app2"><i class="bx bx-support"></i></span>
@@ -15,20 +14,20 @@
 
           <div class="product">
             <router-link to="/erp">
-              <span class="app2"><i class='bx bx-cog'></i></span>
+              <span class="app2"><i class="bx bx-cog"></i></span>
               <p>ERP</p>
             </router-link>
           </div>
 
           <div class="product">
             <router-link to="/human-management">
-              <span class="app2"><i class='bx bxs-user-circle'></i></span>
+              <span class="app2"><i class="bx bxs-user-circle"></i></span>
               <p>Human Resources</p>
             </router-link>
           </div>
-        </div>
 
-        <div class="products">
+
+
           <div class="product">
             <router-link to="/sales">
               <span class="app2"><i class="bx bx-trending-up"></i></span>
@@ -45,54 +44,53 @@
 
           <div class="product">
             <router-link to="/ecommerce">
-              <span class="app2"><i class='bx bxs-shopping-bags'></i></span>
+              <span class="app2"><i class="bx bxs-shopping-bags"></i></span>
               <p>E-commerce</p>
             </router-link>
           </div>
-        </div>
 
-         <div class="products">
+
+
           <div class="product">
             <router-link to="/websites">
-              <span class="app2"><i class='bx bxl-html5'></i></span>
+              <span class="app2"><i class="bx bxl-html5"></i></span>
               <p>Websites</p>
             </router-link>
           </div>
 
           <div class="product">
             <router-link to="/analytics">
-              <span class="app2"><i class='bx bx-line-chart-down'></i></span>
+              <span class="app2"><i class="bx bx-line-chart-down"></i></span>
               <p>Analytics</p>
             </router-link>
           </div>
 
           <div class="product">
             <router-link to="/business-intelligence">
-              <span class="app2"><i class='bx bxs-briefcase-alt-2'></i></span>
+              <span class="app2"><i class="bx bxs-briefcase-alt-2"></i></span>
               <p>Business Intelligence</p>
             </router-link>
           </div>
-        </div>
       </div>
-      </div>
+    </div>
   </section>
 </template>
 
 <script>
 export default {
-mounted() {
+  mounted() {
     document.title = "Products | ColossalHub";
   },
-}
+};
 </script>
 
 <style scoped>
 .section {
-   background: rgba(185, 207, 236, 0.897);
+  background: rgba(185, 207, 236, 0.897);
 }
 .app2 {
   font-size: 4rem;
-  padding: .5rem;
+  padding: 0.5rem;
   border-radius: 1rem;
   background: var(--white);
 }
@@ -109,9 +107,16 @@ mounted() {
   text-align: center;
   font-style: italic;
 }
-.products {
-  display: flex;
-  align-items: center;
-  justify-content: space-around;
+
+.products-page {
+  grid-template-columns: repeat(3, 1fr);
+}
+
+/*==== RESPONSIVE DESIGN ==== */
+
+@media only screen and (max-width: 450px) {
+  .products-page {
+    grid-template-columns: repeat(2, 1fr);
+  }
 }
 </style>
