c4f9da790f8e8cb33029f2fbacfd2da5c5d7eafc
sicelo17 <70627871+sicelo17@users.noreply.github.com>
2021-10-10 00:27:51 +0200

    store init

--- src/store/index.js
+++ src/store/index.js
@@ -2,8 +2,19 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    isAuthenticated: false,
+    token: '',
   },
   mutations: {
+
+    setToken(state, token) {
+      state.token = token
+      state.isAuthenticated = true
+    },
+    removeToken(state) {
+      state.token = ''
+      state.isAuthenticated = false
+    },
   },
   actions: {
   },
