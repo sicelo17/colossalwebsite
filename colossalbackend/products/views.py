7cb2bc105ab808df082b2be6d3937928c018e8c4
sicelo17 <70627871+sicelo17@users.noreply.github.com>
2021-10-21 23:47:30 +0200

    created product view

--- colossalbackend/products/views.py
+++ colossalbackend/products/views.py
@@ -13,3 +13,15 @@ class LatestProductsList(APIView):
         products = Product.objects.all()[0:4]  #getting the first four items from the database
         serializer = ProductSerializer(products, many=True)
         return Response(serializer.data)
+
+class ProductDetail(APIView):
+    def get_object(self, category_slug, product_slug):
+        try:
+            return Product.objects.filter(category__slug=category_slug).get(slug=product_slug)
+        except Product.DoesNotExist:
+            raise Http404
+
+    def get(self, request, category_slug, product_slug, format=None):
+        product = self.get_object(category_slug, product_slug)
+        serializer = ProductSerializer(product)     #getting data from serializers.py
+        return Response(serializer.data)
