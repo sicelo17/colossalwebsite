1e02a902288bca4c5b22a14613001b400b374bee
sicelo17 <70627871+sicelo17@users.noreply.github.com>
2021-11-02 11:15:28 +0200

    added industry views

--- colossalbackend/industries/views.py
+++ colossalbackend/industries/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.db.models import Q 
+from django.http import Http404
 
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.decorators import api_view 
+
+from .models import Product, Category, Industry
+from .serializers import CategorySerializer, IndustrySerializer
+
+
+class ProductList(APIView):
+    def get(self, request, format=None):
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
