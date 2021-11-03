3a98c3d58c64b3266ca5422e17f86a17f098603e
sicelo17 <70627871+sicelo17@users.noreply.github.com>
2021-11-03 11:27:59 +0200

    added signup methods

--- colossalvue/src/views/Signup.vue
+++ colossalvue/src/views/Signup.vue
@@ -91,55 +91,53 @@ export default {
     document.title = "Sign Up | ColossalHub";
   },
   methods: {
-    async submitForm() {
-      this.errors = [];
-      if (this.username === "") {
-        this.errors.push("The username is missing");
-      }
-      if (this.password === "") {
-        this.errors.push("The password is too short");
-      }
-      if (this.password !== this.password2) {
-        this.errors.push("The passwords doesn't match");
-      }
-      if (!this.errors.length) {
-        const formData = {
-          email: this.email,
-          username: this.username,
-          password: this.password,
-          password2: this.password2,
-        };
-        axios
-          .post("https://apidjackets.codewithstein.com/api/v1/users/", formData)
-          .then((response) => {
-            toast({
-              message: "Account created, please log in!",
-              type: "is-success",
-              dismissible: true,
-              pauseOnHover: true,
-              duration: 2000,
-              position: "center",
-            });
-            console.log(response);
-            this.$router.push("/login");
-          })
-          .catch((error) => {
-            if (error.response) {
-              for (const property in error.response.data) {
-                this.errors.push(
-                  `${property}: ${error.response.data[property]}`
-                );
-              }
-              console.log(JSON.stringify(error.response.data));
-            } else if (error.message) {
-              this.errors.push("Something went wrong. Please try again");
-
-              console.log(JSON.stringify(error));
+    submitForm() {
+            this.errors = []
+            if (this.email === ''){
+              this.error.push('The email is missing')
             }
-          });
-      }
-    },
-  },
+            if (this.username === '') {
+                this.errors.push('The username is missing')
+            }
+            if (this.password === '') {
+                this.errors.push('The password is too short')
+            }
+            if (this.password !== this.password2) {
+                this.errors.push('The passwords doesn\'t match')
+            }
+            if (!this.errors.length) {
+                const formData = {
+                    username: this.username,
+                    password: this.password
+                }
+                axios
+                    .post("/api/v1/users/", formData)
+                    .then(response => {
+                        toast({
+                            message: 'Account created, please log in!',
+                            type: 'is-success',
+                            dismissible: true,
+                            pauseOnHover: true,
+                            duration: 2000,
+                            position: 'bottom-right',
+                        })
+                        this.$router.push('/login')
+                    })
+                    .catch(error => {
+                        if (error.response) {
+                            for (const property in error.response.data) {
+                                this.errors.push(`${property}: ${error.response.data[property]}`)
+                            }
+                            console.log(JSON.stringify(error.response.data))
+                        } else if (error.message) {
+                            this.errors.push('Something went wrong. Please try again')
+                            
+                            console.log(JSON.stringify(error))
+                        }
+                    })
+            }
+        }
+    }
 };
 </script>
 
